8361a4ba66917bce86330e4427e8b25be0dd885e
badung7576 <dung.dang@pirago.work>
2021-04-13 09:37:37 +0700

    Line follower robot with pid control

--- robot_do_line/robot_do_line/robot_do_line.ino
+++ robot_do_line/robot_do_line/robot_do_line.ino
@@ -1,239 +1,124 @@
-/*
-  Analog input, analog output, serial output
-
- Reads an analog input pin, maps the result to a range from 0 to 255
- and uses the result to set the pulsewidth modulation (PWM) of an output pin.
- Also prints the results to the serial monitor.
-
- The circuit:
- * potentiometer connected to analog pin 0.
-   Center pin of the potentiometer goes to the analog pin.
-   side pins of the potentiometer go to +5V and ground
- * LED connected from digital pin 9 to ground
-
- created 29 Dec. 2008
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
-
-// These constants won't change.  They're used to give names
-// to the pins used:
-const int EnA = 11;  // Analog input pin that the potentiometer is attached to
-const int EnB = 10; // Analog output pin that the LED is attached to
+const int EnA = 11; // right
+const int EnB = 10; // left
 const int InA = 13;
 const int InB = 12;
 const int InC = 9;
 const int InD = 8;
 
-const int Pin_ss1 = A4; // Analog output pin that the LED is attached to
-const int Pin_ss2 = A3;
-const int Pin_ss3 = A2;
-const int Pin_ss4 = A1;
-const int Pin_ss5 = A0;
+int error, previous_error;
 
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
+const int MAX_SPEED = 80;
+int mask;
+const int NUM_SENSORS = 5; 
+const int sensor_pins[NUM_SENSORS] = {A4, A3, A2, A1, A0};
 
-int IN_line,In_line_last,mode;
-int ss1,ss2,ss3,ss4,ss5;
+float Kp = 12;
+float Kd = 0.8;
+float Ki = 0;
+
+int integral = 0;
+int rightSpeedPwm = 0;
+int leftSpeedPwm = 0;
+
+void getSensor();
+void control_motor(int left, int right);
+void control_robot(int error);
+float calculate_pid();
 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  pinMode(InA, OUTPUT);
-  pinMode(InB, OUTPUT);
-  pinMode(InC, OUTPUT);
-  pinMode(InD, OUTPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(InA, LOW);
-  digitalWrite(InB, HIGH);
-  digitalWrite(InC, LOW);
-  digitalWrite(InD, HIGH);
-  digitalWrite(13, LOW);
-  analogWrite(EnA,255);
-  analogWrite(EnB,255);
-  pinMode(Pin_ss1, INPUT);//Set chân cảm biến 1 là input
-  pinMode(Pin_ss2, INPUT);//Set chân cảm biến 2 là input
-  pinMode(Pin_ss3, INPUT);//Set chân cảm biến 3 là input
-  pinMode(Pin_ss4, INPUT);//Set chân cảm biến 4 là input
-  pinMode(Pin_ss5, INPUT);//Set chân cảm biến 4 là input
-  //Serial.begin(9600);
+    
+    pinMode(InA, OUTPUT);
+    pinMode(InB, OUTPUT);
+    pinMode(InC, OUTPUT);
+    pinMode(InD, OUTPUT);
+    digitalWrite(InA, LOW);
+    digitalWrite(InB, HIGH);
+    digitalWrite(InC, LOW);
+    digitalWrite(InD, HIGH);
+    
+    analogWrite(EnA, MAX_SPEED);
+    analogWrite(EnB, MAX_SPEED);
+    
+    for(int i = 0; i < NUM_SENSORS; i++) {
+        pinMode(sensor_pins[i], INPUT);
+    }
+
+    Serial.begin(9600);
 }
 
 void loop() {
-  In_line_last = IN_line;
-  IN_line = In_SenSor();
-  if(IN_line!=In_line_last)
-  {
-    if(In_line_last!=5)
-    {
-      robotMover(IN_line);
-    } 
-  }
+    getSensor();
+    float pid = calculate_pid();
+    Serial.println(">>>>Error: " + String(error) + "    >>>>PID: " + String(pid));
+    int leftSpeedPwm = MAX_SPEED + pid; 
+    int rightSpeedPwm = MAX_SPEED - pid;
+    
+    leftSpeedPwm = constrain(leftSpeedPwm, 0, 255);
+    rightSpeedPwm = constrain(rightSpeedPwm, 0, 255);
+    Serial.println(">>>>>>" + String(leftSpeedPwm) + "====" + String(rightSpeedPwm) + "<<<<<<<");
+
+    control_motor(leftSpeedPwm, rightSpeedPwm);
 }
-void robotMover (int line)
-{
-  switch (line)
-  {
-    case -4:// sang trai
-    {
-      analogWrite(EnA,150);//banh phai
-      analogWrite(EnB,25);//banh trai
-      //delay(200);
-      break;
+
+void getSensor() {
+    mask ^= mask;
+    for(int i = 0; i < NUM_SENSORS; i++) {
+        mask |= digitalRead(sensor_pins[i]) << i;
     }
-    case -3://sang trai
-    {
-      analogWrite(EnA,100);//banh phai
-      analogWrite(EnB,25);//banh trai
-      //delay(50);
-      break;
+
+    switch (mask) { 
+        case 0b00001 : error = -4; break;       // Left deviation
+        case 0b00011 : error = -3; break;
+        case 0b00010 : error = -2; break;
+        case 0b00110 : error = -1; break;
+
+        case 0b00100 : error = 0; break;        // Center 
+
+        case 0b01100 : error = 1; break;        // Right deviation
+        case 0b01000 : error = 2; break;
+        case 0b11000 : error = 3; break;
+        case 0b10000 : error = 4; break;
+
+        case 0b00000 :                          // Left or right deviation
+            if(error < 0) error = -5;
+            else error = 5;
+            break;
+        default: break; 
     }
-    case -2:// sang trai
-    {
-      analogWrite(EnA,100);//banh phai
-      analogWrite(EnB,50);//banh trai
-      //delay(30);
-      break;
-    };
-    case -1:// sang trai
-    {
-      analogWrite(EnA,150);//banh phai
-      analogWrite(EnB,100);//banh trai
-      //delay(10);
-      break;
-    }
-    case 0:// quay phải
-    {
-      analogWrite(EnA,150);//banh phai
-      analogWrite(EnB,150);//banh trai
-      break;
-    }
-    case 1:// rẽ phai
-    {
-      analogWrite(EnA,100);//banh phai
-      analogWrite(EnB,150);//banh trai
-      //delay(10);
-      break;
-    };
-    case 2:// rẽ phai
-    {
-      analogWrite(EnA,50);//banh phai
-      analogWrite(EnB,100);//banh trai
-     // delay(30);
-      break;
-    }
-    case 3:// rẽ phai
-    {
-      analogWrite(EnA,25);//banh phai
-      analogWrite(EnB,100);//banh trai
-      //delay(50);
-      break;
-    }
-    case 4:// rẽ phai
-    {
-      analogWrite(EnA,25);//banh phai
-      analogWrite(EnB,150);//banh tra
-     // delay(200);
-      break;
-    }
-    case 5:// rẽ phải
-    {
-      Serial.print(mode);
-      if(mode==0)
-      {
-        analogWrite(EnA,80);//banh phai
-        analogWrite(EnB,0);//banh tra
-      }
-      else
-      {
-        analogWrite(EnA,0);//banh phai
-        analogWrite(EnB,80);//banh tra
-      }
-      break;
-    }
-    case 6:{
-      analogWrite(EnA,80);//banh phai
-      analogWrite(EnB,0);//banh tra
-      //delay(1000);
-      break;
-    }
-    case 7:{
-      analogWrite(EnA,0);//banh phai
-      analogWrite(EnB,80);//banh tra
-      //delay(1000);
-      break;
-    }
-  }
 }
-int In_SenSor()
-{
-  ss1 = digitalRead(Pin_ss1);
-  ss2 = digitalRead(Pin_ss2);
-  ss3 = digitalRead(Pin_ss3);
-  ss4 = digitalRead(Pin_ss4);
-  ss5 = digitalRead(Pin_ss5);
-  
-  if((ss1==1)&&(ss2==1)&&(ss3==0)&&(ss4==1)&&(ss5==1))//giua line
-  {
-    return 0; 
-  }
-  else if((ss1==1)&&(ss2==1)&&(ss3==0)&&(ss4==0)&&(ss5==1))//lech trai cap 1
-  {
-    mode=1;
-    return 1;
-  }
-  else if((ss1==1)&&(ss2==1)&&(ss3==1)&&(ss4==0)&&(ss5==1))//lech trai cap 2
-  {
-    mode=1;
-    return 2;
-  }
-  else if((ss1==1)&&(ss2==1)&&(ss3==1)&&(ss4==0)&&(ss5==0))//lech trai cap 3
-  {
-    mode=1;
-    return 3;
-  }
-  else if((ss1==1)&&(ss2==1)&&(ss3==1)&&(ss4==1)&&(ss5==0))//lech trai cap 4////////////////
-  {
-    mode=1;
-    return 4;
-  }
-  else if((ss1==1)&&(ss2==0)&&(ss3==0)&&(ss4==1)&&(ss5==1))//lech phai cap 1
-  {
-    mode=0;
-    return -1;
-  }
-  else if((ss1==1)&&(ss2==0)&&(ss3==1)&&(ss4==1)&&(ss5==1))//lech phai cap 2
-  {
-    mode=0;
-    return -2;
-  }
-  else if((ss1==0)&&(ss2==0)&&(ss3==1)&&(ss4==1)&&(ss5==1))//lech phai cap 3
-  {
-    mode=0;
-    return -3;
-  }
-  else if((ss1==0)&&(ss2==1)&&(ss3==1)&&(ss4==1)&&(ss5==1))//lech phai cap 4
-  {
-    mode=0;
-    return -4;
-  }
-  else if((ss1==1)&&(ss2==1)&&(ss3==1)&&(ss4==1)&&(ss5==1))//lech hoàn toàn
-  {
-    return 5;
-  }
-  else if((ss1==0)&&(ss2==0)&&(ss3==0)&&(ss5==1))//góc vuong sang trai
-  {
-    mode=0;
-    return 6;
-  }
-  else if((ss1==1)&&(ss3==0)&&(ss4==0)&&(ss5==0))//góc vuong sang phai
-  {
-    mode=1;
-    return 7;
-  }
-  
-  
+
+void control_motor(int left, int right) {
+    analogWrite(EnA, right);
+    analogWrite(EnB, left);
+}
+
+// void control_robot(int error) {
+//     switch (error) {
+//         case -4: control_motor(25, 120);  break;
+//         case -3: control_motor(25, 100);  break;
+//         case -2: control_motor(50, 100);  break;
+//         case -1: control_motor(100, 150); break;
+//         case  0: control_motor(150, 150); break;
+//         case  1: control_motor(150, 100); break;
+//         case  2: control_motor(100, 50);  break;
+//         case  3: control_motor(100, 25);  break;
+//         case  4: control_motor(150, 25);  break;
+//         case  6: control_motor(0, 80);    break;
+//         case  7: control_motor(80, 0); break;
+//         case  5: 
+//             if(is_left_direction == 0)
+//                 control_motor(0, 80); 
+//             else
+//                 control_motor(80, 0);    
+//             break;   
+//         default: break;
+//     }
+// }
+
+
+float calculate_pid() {
+    integral = integral + error;
+    int pid = Kp * error + Kd * (error - previous_error) + Ki * integral;
+    previous_error = error;
+    return pid;
 }
